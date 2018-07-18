16c6ab82183c64ec78240dcbdc6d86aecb3b5842
mtmmy <mtmmy@users.noreply.github.com>
2018-07-18 15:27:43 -0700

    # Updates: 1. Code refactor 2. Add wrap characters to make paragraph working

--- doc-generator.py
+++ doc-generator.py
@@ -17,8 +17,8 @@ WAIT = WebDriverWait(CODE_DRIVER,10)
 LEETCODE_PROBLEMS = []
 
 class Problem (object):
-    def __init__ (self, problemNo, title, href, acceptance, difficulty ):
-        self.problemNo = problemNo
+    def __init__ (self, number, title, href, acceptance, difficulty ):
+        self.number = number
         self.title = title
         self.href = href
         self.acceptance = acceptance
@@ -42,14 +42,14 @@ def sign_into_leetcode():
     password = input("Enter your password: ")
 
     CODE_DRIVER.get("https://leetcode.com/accounts/login/")
-    CODE_DRIVER.find_element_by_xpath('// *[ @ id = "id_login"]').send_keys(username)
-    CODE_DRIVER.find_element_by_xpath('// *[ @ id = "id_password"]').send_keys(password)
-    CODE_DRIVER.find_element_by_xpath('// *[ @ id = "id_password"]').send_keys(Keys.ENTER)
+    CODE_DRIVER.find_element_by_css_selector("#id_login").send_keys(username)
+    CODE_DRIVER.find_element_by_css_selector("#id_password").send_keys(password)
+    CODE_DRIVER.find_element_by_css_selector("#id_password").send_keys(Keys.ENTER)
         
     CODE_DRIVER.implicitly_wait(0)
     time.sleep(5)
 
-def check_element_exist_by_class_name(class_name):
+def go_to_next_page(class_name):
     try:
         element = CODE_DRIVER.find_element_by_class_name(class_name)
         element.click()        
@@ -60,12 +60,12 @@ def check_element_exist_by_class_name(class_name):
 def get_problem_rows(table):
     problems = []
 
-    for row in table.find_elements_by_css_selector('tbody:nth-of-type(1)>tr'):
-        number = row.find_element_by_css_selector('td:nth-of-type(2)').text
-        title = row.find_element_by_css_selector('td:nth-of-type(3) a').text
-        href = row.find_element_by_css_selector('td:nth-of-type(3) a').get_attribute('href')
-        acceptance = row.find_element_by_css_selector('td:nth-of-type(5)').text
-        difficulty = row.find_element_by_css_selector('td:nth-of-type(6) span').text
+    for row in table.find_elements_by_css_selector("tbody:nth-of-type(1)>tr"):
+        number = row.find_element_by_css_selector("td:nth-of-type(2)").text
+        title = row.find_element_by_css_selector("td:nth-of-type(3) a").text
+        href = row.find_element_by_css_selector("td:nth-of-type(3) a").get_attribute('href')
+        acceptance = row.find_element_by_css_selector("td:nth-of-type(5)").text
+        difficulty = row.find_element_by_css_selector("td:nth-of-type(6) span").text
         problem = Problem(int(number), title, href, acceptance, difficulty)
         problems.append(problem)
     return problems
@@ -76,71 +76,73 @@ def scrap_description():
 
     CODE_DRIVER.get("https://leetcode.com/problemset/algorithms/?status=Solved")
 
-    table = CODE_DRIVER.find_element_by_css_selector('.question-list-table>table')
+    table = CODE_DRIVER.find_element_by_css_selector(".question-list-table>table")
 
     global LEETCODE_PROBLEMS
     LEETCODE_PROBLEMS += get_problem_rows(table)
 
-    while check_element_exist_by_class_name("reactable-next-page"):        
+    while go_to_next_page("reactable-next-page"):        
         LEETCODE_PROBLEMS += get_problem_rows(table)    
 
-    csharp_path = '/Users/mtmmy/Projects/Leetcode/Csharp/Leetcode/'
+    csharp_path = "/Users/mtmmy/Projects/Leetcode/Csharp/Leetcode/"
     folders = get_folders(csharp_path)
     
     for problem in LEETCODE_PROBLEMS:
-        if problem.problemNo in folders:
-            problem_path = csharp_path + folders[problem.problemNo]
-            file_path = problem_path + '/README.md'
+        if problem.number in folders:
+            problem_path = csharp_path + folders[problem.number]
+            file_path = problem_path + "/README.md"
             
             if not os.path.isfile(file_path):
                 CODE_DRIVER.get(problem.href)
                 description = WAIT.until(EC.presence_of_element_located((By.CLASS_NAME, "question-description__3U1T")))
-                children = description.find_element_by_tag_name('div').find_elements_by_xpath('*')            
+                children = description.find_element_by_tag_name("div").find_elements_by_xpath("*")            
             
                 file = open(file_path, 'w')
-                file.write("# [" + str(problem.problemNo) + ". " + problem.title + "](" + problem.href + ")\n\n")
-                file.write("## Description\n\n")
+                file.write("# [" + str(problem.number) + ". " + problem.title + "](" + problem.href + ")\n\n")
+                file.write("## Description\n")
                 for child in children:
-                    if child.tag_name == 'p':
+                    if child.tag_name == "p":
+                        file.write("\n")
                         file.write(child.text)
-                        file.write('\n')
-                    if child.tag_name == 'pre':
-                        file.write('```')
-                        file.write('\n')
+                        file.write("\n")
+                    if child.tag_name == "pre":
+                        file.write("\n")
+                        file.write("```")
+                        file.write("\n")
                         file.write(child.text)
-                        file.write('\n')
-                        file.write('```')
-                        file.write('\n')
-                file.write("## Solution\n\n")
+                        file.write("\n")
+                        file.write("```")
+                        file.write("\n")
+                file.write("\n## Solution\n\n")
                 file.close()
 
 def create_sum_file():
     global LEETCODE_PROBLEMS
     csharp_github_url = "https://github.com/mtmmy/Leetcode/tree/master/Csharp/Leetcode/"
-    csharp_local_path = '/Users/mtmmy/Projects/Leetcode/Csharp/Leetcode/'
+    csharp_local_path = "/Users/mtmmy/Projects/Leetcode/Csharp/Leetcode/"
     folders = get_folders(csharp_local_path)
     leetcode_path = "/Users/mtmmy/Projects/Leetcode/"
     sum_file_path = leetcode_path + "README.md"
 
-    LEETCODE_PROBLEMS.sort(key=lambda x: x.problemNo, reverse=False)
+    LEETCODE_PROBLEMS.sort(key=lambda x: x.number, reverse=False)
 
     file = open(sum_file_path, 'w')
     file.write("# Leetcode Solutions\n\n")
     file.write("This repository shows all of leetcode problems I'd accomplished. I use C# as my primary language to solve problems because I want to get familiar with it by utilizing it more but sometimes I use Java as well.\n\n")
-    file.write('| # | Title | Acceptance | Difficulty | Language | \n')
-    file.write('|:---:|:---|:---:|:---:|:---:|\n')
+    file.write("| # | Title | Acceptance | Difficulty | Language | \n")
+    file.write("|:---:|:---|:---:|:---:|:---:|\n")
 
     for p in LEETCODE_PROBLEMS:
-        if p.problemNo in folders:
-            folder_name = folders[p.problemNo]
+        if p.number in folders:
+            folder_name = folders[p.number]
             data = {
-                'num': p.problemNo,
-                'title': '[' + p.title + '](' + csharp_github_url + folder_name + ')',
+                'num': p.number,
+                'title': '[' + p.title + "](" + csharp_github_url + folder_name + ")",
                 'acceptance': p.acceptance,
                 'difficulty': p.difficulty,
-                'language': 'C#'
+                'language': "C#"
             }
-            line = '|{num}|{title}|{acceptance}|{difficulty}|{language}|\n'.format(**data)
+            line = "|{num}|{title}|{acceptance}|{difficulty}|{language}|\n".format(**data)
             file.write(line)
             
     
