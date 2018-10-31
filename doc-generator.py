5816cf6677c7f25e29f28e862ae802d35311f0e0
mtmmy <mtmmy@users.noreply.github.com>
2018-10-31 15:56:25 -0700

    scrap related topics and similar questions

--- doc-generator.py
+++ doc-generator.py
@@ -4,6 +4,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import WebDriverException
 import time
 import os
 import re
@@ -16,6 +17,7 @@ WAIT = WebDriverWait(CODE_DRIVER,10)
 
 LEETCODE_PROBLEMS = []
 
+LEETCODE_URL = "https://leetcode.com"
 GITHUB_URL = "https://github.com/mtmmy/Leetcode/tree/master/"
 GITHUB_URL_CSHARP = GITHUB_URL + "Csharp/Leetcode/"
 GITHUB_URL_JAVA = GITHUB_URL + "Java/Leetcode/src/main/java/com/leetcode/"
@@ -124,11 +126,38 @@ def create_read_me(type):
             
             CODE_DRIVER.get(problem.href)
             allDesc = WAIT.until(EC.presence_of_element_located((By.CLASS_NAME, "description__3vkv")))
-            description = allDesc.find_element_by_class_name("content__1c40")
-            topics = allDesc.find_element_by_class_name("topic-tag__Hn49")
-            simQs = allDesc.find_element_by_class_name("question__3owm")
+            # description = allDesc.find_element_by_class_name("content__1c40")
+            description = WAIT.until(EC.presence_of_element_located((By.CLASS_NAME, "content__1c40")))
 
-            file = None
+            # btnClickable = False
+            
+
+            
+            # while not btnClickable and btns:
+            #     try:
+            #         btns[0].click()
+            #         btnClickable = True
+            #     except WebDriverException:
+            #         print("Still not clickable. Keep waiting")
+            #         time.sleep(0.1)
+
+            # for btn in btns[0:]:
+            #     btn.click()        
+
+            topics = allDesc.find_elements_by_class_name("topic-tag__Hn49")
+            simQs = allDesc.find_elements_by_class_name("title__3BnH")
+
+            btns = allDesc.find_elements_by_class_name("css-1dhf2dg-baseHeaderStyle")
+            
+            for btn in btns:
+                while 1:
+                    try:
+                        btn.click()
+                        time.sleep(0.5)
+                        break
+                    except WebDriverException:
+                        time.sleep(0.5)
+
             if not os.path.isfile(file_path):
                             
                 children = description.find_element_by_tag_name("div").find_elements_by_xpath("*")
@@ -158,14 +187,48 @@ def create_read_me(type):
                         file.write("\n")
                         file.write("```")
                         file.write("\n")
+                        
                 file.write("\n## Solution\n\n")
-                # file.close()
+
+                file.write("## Related Topics")
+                topicsLine = ""
+                for topic in topics:
+                    topiscUrl = LEETCODE_URL + topic.get_attribute("href")
+                    topicTxt = topic.find_element_by_class_name("tag__2PqS").text
+                    topicsLine += "[" + topicTxt + "](" + topiscUrl + "), "
+
+                file.write("## Similar Questions")
+                simQsLine = ""
+                for q in simQs:
+                    topiscUrl = LEETCODE_URL + q.get_attribute("href")
+                    topicTxt = q.text
+                    topicsLine += "[" + topicTxt + "](" + topiscUrl + "), "
+                file.write(simQsLine)
+                file.close()
             else:
-                file = open(file_path, 'a')
-            
+                loadFile = open(file_path).read()
 
-
-            file.close()
+                with open(file_path, 'a') as myfile:
+                    if "Related Topics" not in loadFile:
+                        # myfile.write("\n## Related Topics\n\n")
+                        topicsLine = ""
+                        for topic in topics:
+                            tipicUrl = LEETCODE_URL + topic.get_attribute("href")
+                            topicTxt = topic.find_element_by_class_name("tag__2PqS").text
+                            topicsLine += "[" + topicTxt + "](" + tipicUrl + "), "
+                        # file.write(topicsLine)
+                        # myfile.write("\n")
+                    
+                    if "Similar Questions" not in loadFile:
+                        # myfile.write("\n## Similar Questions\n\n")
+                        simQsLine = ""
+                        for q in simQs:
+                            simQsLine += "[" + q.text + "](" + LEETCODE_URL + q.get_attribute("href") + "), "
+                        # file.write(simQsLine)
+                        # myfile.write("\n")
+                        
+                print("")
+            # file.close()
 
 
 def scrap_description():
