db1468db4ba239b21368704f91be135d0232e1b0
mtmmy <mtmmy@users.noreply.github.com>
2018-11-01 14:32:43 -0700

    Final version of scraping related topics and similar questions

--- doc-generator.py
+++ doc-generator.py
@@ -17,7 +17,6 @@ WAIT = WebDriverWait(CODE_DRIVER,10)
 
 LEETCODE_PROBLEMS = []
 
-LEETCODE_URL = "https://leetcode.com"
 GITHUB_URL = "https://github.com/mtmmy/Leetcode/tree/master/"
 GITHUB_URL_CSHARP = GITHUB_URL + "Csharp/Leetcode/"
 GITHUB_URL_JAVA = GITHUB_URL + "Java/Leetcode/src/main/java/com/leetcode/"
@@ -44,7 +43,7 @@ class Problem (object):
 
 def get_folders(type, path):
     """
-    get folders form local, need to be change to fit your folder structure
+    get folders from local, need to be change to fit your folder structure
     """
     folders_dictionary = {}
     for p in os.listdir(path):
@@ -65,7 +64,6 @@ def get_folders(type, path):
 def sign_into_leetcode():
 
     CODE_DRIVER.implicitly_wait(TIME_DELAY)
-
     CODE_DRIVER.get("https://leetcode.com/accounts/logout")
 
     username = input("Enter your username: ")
@@ -104,13 +102,61 @@ def get_problem_rows(table):
         problems.append(problem)
     return problems
 
+def click_extra_info(probDesc):
+    btns = probDesc.find_elements_by_class_name("css-1dhf2dg-baseHeaderStyle")
+    for btn in btns:
+        while 1:
+            try:
+                btn.click()
+                time.sleep(0.1)
+                break
+            except WebDriverException:
+                time.sleep(0.5)
+
+def write_description(element, myFile):
+    if element.tag_name == "p":
+        txt = element.text.strip()
+        if txt:
+            try:
+                CODE_DRIVER.implicitly_wait(0)
+                image = element.find_element_by_tag_name("img")
+                CODE_DRIVER.implicitly_wait(TIME_DELAY)
+                myFile.write("\n")
+                myFile.write("![image](" + image.get_attribute("src") + ")")
+                myFile.write("\n")
+            except NoSuchElementException:
+                CODE_DRIVER.implicitly_wait(TIME_DELAY)
+            myFile.write("\n")
+            myFile.write(element.text)
+            myFile.write("\n")
+    elif element.tag_name == "pre":
+        myFile.write("\n")
+        myFile.write("```")
+        myFile.write("\n")
+        myFile.write(element.text)
+        myFile.write("\n")
+        myFile.write("```")
+        myFile.write("\n")
+    elif element.tag_name == "div":
+        # get descriptions recursively
+        children = element.find_elements_by_xpath("*")
+        for child in children:
+            write_description(child, myFile)
+    elif element.tag_name == "ol":
+        children = element.find_elements_by_xpath(".//li")
+        myFile.write("\n")
+        for i in range(len(children)):
+            myFile.write(str(i + 1) + ". " + children[i].text + "\n")
+    else:
+        # to track if unconsidered tags matter
+        print("Some new tags: " + element.tag_name)
+        return
+
 def create_read_me(type):
     """
     create README.md for each problem under its folder
     """
-
     local_path = ""
-    
     if type == "CSHARP":
         local_path = LOCAL_PATH_CSHARP
     elif type == "JAVA":
@@ -123,126 +169,84 @@ def create_read_me(type):
     for problem in LEETCODE_PROBLEMS:
         if problem.number in folders:
             file_path = local_path + folders[problem.number] + "/README.md"
-            
-            CODE_DRIVER.get(problem.href)
-            allDesc = WAIT.until(EC.presence_of_element_located((By.CLASS_NAME, "description__3vkv")))
-            # description = allDesc.find_element_by_class_name("content__1c40")
-            description = WAIT.until(EC.presence_of_element_located((By.CLASS_NAME, "content__1c40")))
-
-            # btnClickable = False
-            
-
-            
-            # while not btnClickable and btns:
-            #     try:
-            #         btns[0].click()
-            #         btnClickable = True
-            #     except WebDriverException:
-            #         print("Still not clickable. Keep waiting")
-            #         time.sleep(0.1)
-
-            # for btn in btns[0:]:
-            #     btn.click()        
-
-            topics = allDesc.find_elements_by_class_name("topic-tag__Hn49")
-            simQs = allDesc.find_elements_by_class_name("title__3BnH")
-
-            btns = allDesc.find_elements_by_class_name("css-1dhf2dg-baseHeaderStyle")
-            
-            for btn in btns:
-                while 1:
-                    try:
-                        btn.click()
-                        time.sleep(0.5)
-                        break
-                    except WebDriverException:
-                        time.sleep(0.5)
-
             if not os.path.isfile(file_path):
-                            
+                CODE_DRIVER.get(problem.href)
+                allDesc = WAIT.until(EC.presence_of_element_located((By.CLASS_NAME, "description__3vkv")))
+                CODE_DRIVER.implicitly_wait(0)
+                description = WAIT.until(EC.presence_of_element_located((By.CLASS_NAME, "content__1c40"))) 
+
+                topics = allDesc.find_elements_by_class_name("topic-tag__Hn49")
+                simQs = allDesc.find_elements_by_class_name("title__3BnH")
+
+                click_extra_info(allDesc)
                 children = description.find_element_by_tag_name("div").find_elements_by_xpath("*")
 
-                file = open(file_path, 'w')
-                file.write("# [" + str(problem.number) + ". " + problem.title + "](" + problem.href + ")\n\n")
-                file.write("## Description\n")
-                for child in children:
-                    if child.tag_name == "p":
-                        try:
-                            CODE_DRIVER.implicitly_wait(0)
-                            image = child.find_element_by_tag_name("img")
-                            CODE_DRIVER.implicitly_wait(TIME_DELAY)
-                            file.write("\n")
-                            file.write("![image](" + image.get_attribute("src") + ")")
-                            file.write("\n")
-                        except NoSuchElementException:
-                            CODE_DRIVER.implicitly_wait(TIME_DELAY)
-                        file.write("\n")
-                        file.write(child.text)
-                        file.write("\n")
-                    if child.tag_name == "pre":
-                        file.write("\n")
-                        file.write("```")
-                        file.write("\n")
-                        file.write(child.text)
-                        file.write("\n")
-                        file.write("```")
-                        file.write("\n")
-                        
-                file.write("\n## Solution\n\n")
+                with open(file_path, 'w') as myFile:
+                    myFile.write("# [" + str(problem.number) + ". " + problem.title + "](" + problem.href + ")\n\n")
+                    myFile.write("## Description\n")
+                    for child in children:
+                        write_description(child, myFile)
+                            
+                    myFile.write("\n## Solution\n\n")
 
-                file.write("## Related Topics")
-                topicsLine = ""
-                for topic in topics:
-                    topiscUrl = LEETCODE_URL + topic.get_attribute("href")
-                    topicTxt = topic.find_element_by_class_name("tag__2PqS").text
-                    topicsLine += "[" + topicTxt + "](" + topiscUrl + "), "
-
-                file.write("## Similar Questions")
-                simQsLine = ""
-                for q in simQs:
-                    topiscUrl = LEETCODE_URL + q.get_attribute("href")
-                    topicTxt = q.text
-                    topicsLine += "[" + topicTxt + "](" + topiscUrl + "), "
-                file.write(simQsLine)
-                file.close()
-            else:
-                loadFile = open(file_path).read()
-
-                with open(file_path, 'a') as myfile:
-                    if "Related Topics" not in loadFile:
-                        # myfile.write("\n## Related Topics\n\n")
-                        topicsLine = ""
+                    if topics:
+                        myFile.write("## Related Topics\n\n")
+                        topicsPrints = []
                         for topic in topics:
-                            tipicUrl = LEETCODE_URL + topic.get_attribute("href")
+                            tipicUrl = topic.get_attribute("href")
                             topicTxt = topic.find_element_by_class_name("tag__2PqS").text
-                            topicsLine += "[" + topicTxt + "](" + tipicUrl + "), "
-                        # file.write(topicsLine)
-                        # myfile.write("\n")
-                    
-                    if "Similar Questions" not in loadFile:
-                        # myfile.write("\n## Similar Questions\n\n")
-                        simQsLine = ""
+                            topicsPrints.append("[" + topicTxt + "](" + tipicUrl + ") ")
+                        myFile.write(", ".join(topicsPrints))
+                        myFile.write("\n")
+
+                    if simQs:
+                        myFile.write("\n## Similar Questions\n\n")
+                        simQsPrints = []
                         for q in simQs:
-                            simQsLine += "[" + q.text + "](" + LEETCODE_URL + q.get_attribute("href") + "), "
-                        # file.write(simQsLine)
-                        # myfile.write("\n")
+                            simQsPrints.append("[" + q.text + "](" + q.get_attribute("href") + ")")
+                        myFile.write(", ".join(simQsPrints))
+                        myFile.write("\n")
+            # else:
+            #     '''
+            #     This part is only for exsiting files
+            #     '''
+            #     loadFile = open(file_path).read()
+
+            #     if "Related Topics" not in loadFile or "Similar Questions" not in loadFile:
+            #         CODE_DRIVER.get(problem.href)
+            #         allDesc = WAIT.until(EC.presence_of_element_located((By.CLASS_NAME, "description__3vkv")))
+            #         description = WAIT.until(EC.presence_of_element_located((By.CLASS_NAME, "content__1c40"))) 
+
+            #         topics = allDesc.find_elements_by_class_name("topic-tag__Hn49")
+            #         simQs = allDesc.find_elements_by_class_name("title__3BnH")
+
+            #         with open(file_path, 'a') as myFile:
+            #             click_extra_info(allDesc)
+            #             if topics:
+            #                 myFile.write("\n\n## Related Topics\n\n")
+            #                 topicsPrints = []
+            #                 for topic in topics:
+            #                     tipicUrl = topic.get_attribute("href")
+            #                     topicTxt = topic.find_element_by_class_name("tag__2PqS").text
+            #                     topicsPrints.append("[" + topicTxt + "](" + tipicUrl + ") ")
+            #                 myFile.write(", ".join(topicsPrints))                        
                         
-                print("")
-            # file.close()
+            #             if simQs:
+            #                 myFile.write("\n\n## Similar Questions\n\n")
+            #                 simQsPrints = []
+            #                 for q in simQs:
+            #                     simQsPrints.append("[" + q.text + "](" + q.get_attribute("href") + ")")
+            #                 myFile.write(", ".join(simQsPrints))
+            #                 myFile.write("\n")
 
 
 def scrap_description():
-    
     CODE_DRIVER.implicitly_wait(TIME_DELAY)
-
     CODE_DRIVER.get("https://leetcode.com/problemset/algorithms/?status=Solved")
-
     table = CODE_DRIVER.find_element_by_css_selector(".question-list-table>table")
-
     global LEETCODE_PROBLEMS
     LEETCODE_PROBLEMS += get_problem_rows(table)
-
-    while go_to_next_page("reactable-next-page"):        
+    while go_to_next_page("reactable-next-page"):
         LEETCODE_PROBLEMS += get_problem_rows(table)
 
     create_read_me("CSHARP")
